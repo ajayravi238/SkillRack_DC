4269bf933f06e3338f8246cbd1d1a8e8e8b20625
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-05-09 10:05:03 +0530

    Create Basic Calculator

---
+++ BasicCalculator.java
@@ -0,0 +1,22 @@
+package com.skillrack;
+
+public class BasicCalculator {
+    public static int addition(int X, int Y) {
+        return X + Y;
+    }
+    public static int subtraction(int X, int Y) {
+        return X - Y;
+    }
+    public static int multiplication(int X, int Y) {
+        return X * Y;
+    }
+    public static int integerDivision(int X, int Y) {
+        return X / Y;
+    }
+    public static float floatDivision(int X, int Y) {
+        return (float) X / Y;
+    }
+    public static int modulo(int X, int Y) {
+        return X % Y;
+    }
+}
