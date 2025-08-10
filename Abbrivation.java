6cbca23a9aece1c092b9068fb1b4ad8596d670e7
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-08-10 11:04:44 +0530

    Create Abbrivation

---
+++ Abbrivation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Abbrivation
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String[] arr = str.split(" ");
+		for(int i=0; i<arr.length; i++) {
+		    System.out.print(Character.toUpperCase(arr[i].charAt(0)));
+		}
+	}
+}
